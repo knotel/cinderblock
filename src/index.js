c59f7f8422e663b2025df1b0e007f18a2625df48
Will Laeri <wlaeri@gmail.com>
2020-02-25 20:20:52 -0500

    fixed build error

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { Toast, ToastService } from './Toast'
 import InputField from './InputField'
 import useModal from './hooks/useModal'
 import PageHead from './PageHead'
-import Icon from './icons/Icon.jsx'
+import Icon from './icons/Icon'
 import icons from './icons'
 
 const bundle = {
